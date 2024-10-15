c0716e950053a361e1181cab2701de3017bd166a
hiroyuki9614 <mmnnoov@gmail.com>
2024-10-15 23:40:51 +0900

    プロフィールページ

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://hiroyuki9614.github.io/',
+	base: '/my_profile',
+});
