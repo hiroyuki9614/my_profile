177adfbbba1b86fbe769c24a7187ce111b3b38b0
hiroyuki9614 <mmnnoov@gmail.com>
2024-10-18 22:21:58 +0900

    プロフィールページ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hiroyuki9614.github.io/',
-	base: '/my_profile',
+  site: 'https://hiroyuki9614.github.io/',
+  base: '/my_profile',
+  integrations: [tailwind()],
 });
