c4f6a4f212b26a20e83318fcab2d79e3049dde97
hiroyuki9614 <mmnnoov@gmail.com>
2026-05-18 23:05:47 +0900

    260518_marmeid導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import rehypeMermaid from 'rehype-mermaid';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://hiroyuki9614.github.io',
 	base: '/my_profile',
-	integrations: [react(), sitemap()],
+	integrations: [
+		react(),
+		sitemap(),
+		mdx({
+			syntaxHighlight: false,
+			rehypePlugins: [[rehypeMermaid, { strategy: 'inline-svg' }]],
+		}),
+	],
+
+	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['mermaid', 'js'],
+		},
+		rehypePlugins: [[rehypeMermaid, { strategy: 'inline-svg' }]],
+	},
 
 	vite: {
 		plugins: [tailwindcss()],
